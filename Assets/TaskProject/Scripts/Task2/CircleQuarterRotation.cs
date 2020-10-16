c84e5f0b333939d7e70c51e994722eb5884af2e9
GGfrozen <joen4ik@gmail.com>
2020-10-16 13:20:38 +0300

    Task2

---
+++ Assets/TaskProject/Scripts/Task2/CircleQuarterRotation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class CircleQuarterRotation : MonoBehaviour
+{
+    [SerializeField] private Transform circle1;
+    [SerializeField] private Transform circle2;
+
+    private void Start()
+    {
+        CircleRotate();
+    }
+
+    private void CircleRotate()
+    {
+        var duration = 1f;
+        var eulerAngles = new Vector3(0,0,360f);
+        var sequence = DOTween.Sequence();
+        sequence.Append(circle1.DORotate(eulerAngles, duration, RotateMode.FastBeyond360))
+            .Join(circle2.DORotate(-eulerAngles, duration, RotateMode.FastBeyond360))
+            .SetLoops(-1, LoopType.Restart);
+    }
+}
---
+++ Assets/TaskProject/Scripts/Task2/FoldingDuplicate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class FoldingDuplicate : MonoBehaviour
+{
+
+    private float duration = 0.5f;
+    
+
+    private void Start()
+    {
+        var sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScaleX(-1f, duration))
+            .Join(transform.DOScaleY(1f, duration))
+            .Append(transform.DOScaleY(-1f, duration))
+            .Append(transform.DOScaleX(1f, duration))
+            .Append(transform.DOScaleY(1f, duration))
+            .SetLoops(-1);
+        
+    }
+
+}
