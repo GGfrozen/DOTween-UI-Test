42920d0e88d114ee6fbcc79f1502359350d2830e
GGfrozen <joen4ik@gmail.com>
2020-10-16 22:49:41 +0300

    add Task3

--- Assets/TaskProject/Scripts/Task3/UseCards.cs
+++ Assets/TaskProject/Scripts/Task3/UseCards.cs
@@ -3,19 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UseCards : MonoBehaviour
 {
-    private void OnMouseEnter()
-    {
-        var sequence = DOTween.Sequence();
-        var transformPosition = transform.position;
-        transformPosition.x += 1f;
-        transform.position = transformPosition;
-    }
+	[SerializeField] private GameObject[] cards;
+	[SerializeField] private Sprite frontLabel;
+	[SerializeField] float duration = 1f;
+	[SerializeField] private Transform firstHandPosition;
+	
 
-    private void Update()
-    {
-        OnMouseEnter();
-    }
+	private bool isHandFull = false;
+
+	private void Start()
+	{
+		MoveCardToHand();
+	}
+	private void MoveCardToHand()
+	{
+		var eulerAngles = new Vector3(0, 180f, 0);
+		
+		
+		
+		var sequence = DOTween.Sequence();
+		for (var i = 0; i < cards.Length; i++)
+		{
+			var cardInHandPosition = firstHandPosition.position.x + i*100;
+			var backImage = cards[i].GetComponent<Image>();
+			sequence.Append(cards[i].transform.DOMoveX(cardInHandPosition, duration))
+			
+			.Join(cards[i].transform.DORotate(eulerAngles, duration)).AppendCallback(() => backImage.sprite = frontLabel);
+			
+		}
+		
+			
+	}
 }
