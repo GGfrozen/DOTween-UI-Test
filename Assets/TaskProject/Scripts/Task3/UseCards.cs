66c4175b0af1e415fc257dc32201973bd4d21d7c
GGfrozen <joen4ik@gmail.com>
2020-10-16 17:01:50 +0300

    add assets

---
+++ Assets/TaskProject/Scripts/Task3/UseCards.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class UseCards : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        var sequence = DOTween.Sequence();
+        var transformPosition = transform.position;
+        transformPosition.x += 1f;
+        transform.position = transformPosition;
+    }
+
+    private void Update()
+    {
+        OnMouseEnter();
+    }
+}
